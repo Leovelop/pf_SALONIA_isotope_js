040ef1806f048c392b3afbe62f2c8be10000bf3d
Leovelop <saintiscah@gmail.com>
2022-11-03 23:30:09 +0900

    load 이벤트 삭제?

--- js/main.js
+++ js/main.js
@@ -1,89 +1,87 @@
-window.addEventListener("load", () => {
-  /* 전역변수 리스트---------------------------------------- */
-  let grid;
+/* 전역변수 리스트---------------------------------------- */
+let grid;
 
-  const className_on = "on";
+const className_on = "on";
 
-  //header#header
-  const header = document.querySelector("#header");
-  const menu = header.querySelector(".menu");
-  const btns = menu.querySelectorAll("#gnb li");
-  let total = btns.length;
+//header#header
+const header = document.querySelector("#header");
+const menu = header.querySelector(".menu");
+const btns = menu.querySelectorAll("#gnb li");
+let total = btns.length;
 
-  const main = document.querySelector("main");
-  
-  //section#sort
-  const articles = document.querySelectorAll("article");
+const main = document.querySelector("main");
 
-  //section#popUp
-  const popUp = document.querySelector("#popUp");
-  const aside = popUp.querySelector("aside");
-  const close = popUp.querySelector("span");
+//section#sort
+const articles = document.querySelectorAll("article");
+
+//section#popUp
+const popUp = document.querySelector("#popUp");
+const aside = popUp.querySelector("aside");
+const close = popUp.querySelector("span");
 
 
-  /* 이벤트 연결---------------------------------------- */
-  setTimeout(()=> {
-    grid = new Isotope("section", {
-      itemSelection: "article",
-      columnWidth: "article",
-      transitionDuration: "1s",
-    });
-    
-    main.classList.add(className_on);
-  }, 1000);
-  
-  for(let i = 0; i < total; i++) {
-    btns[i].addEventListener("click", e => {
-      e.preventDefault();
-      
-      let isOn = btns[i].classList.contains(className_on);
-      if(isOn) return;
-
-      sortFrame(e);
-      activation(btns, e);
-    })
-  }
-
-  for (let el of articles) {
-    el.addEventListener("click", e => {
-      activePopUp(e);
-    })
-  }
-
-  close.addEventListener("click", () => {
-    popUp.classList.remove("on");
+/* 이벤트 연결---------------------------------------- */
+setTimeout(()=> {
+  grid = new Isotope("section", {
+    itemSelection: "article",
+    columnWidth: "article",
+    transitionDuration: "1s",
   });
+  
+  main.classList.add(className_on);
+}, 1000);
 
+for(let i = 0; i < total; i++) {
+  btns[i].addEventListener("click", e => {
+    e.preventDefault();
+    
+    let isOn = btns[i].classList.contains(className_on);
+    if(isOn) return;
 
-  /* 함수 선언---------------------------------------- */
-  //선택한 메뉴에 .on 추가
-  function activation(lists, active) {
-    for (let el of lists) {
-      el.classList.remove(className_on);
-    }
+    sortFrame(e);
+    activation(btns, e);
+  })
+}
 
-    active.currentTarget.classList.add(className_on);
-  }
+for (let el of articles) {
+  el.addEventListener("click", e => {
+    activePopUp(e);
+  })
+}
 
-  //정렬 함수
-  function sortFrame(target) {
-    const sort = target.currentTarget.querySelector("a").getAttribute("href");
-
-    grid.arrange({
-      filter: sort,
-    });
-  }
-
-  //팝업 함수
-  function activePopUp(target) {
-    let tit = target.currentTarget.querySelector("h2").innerText;
-    let txt = target.currentTarget.querySelector("p").innerText;
-    let src = target.currentTarget.querySelector("img").getAttribute("src");
-
-    aside.querySelector("h2").innerText = tit;
-    aside.querySelector("p").innerText = txt;
-    aside.querySelector("img").setAttribute("src", src);
-
-    popUp.classList.add(className_on);
-  }
+close.addEventListener("click", () => {
+  popUp.classList.remove("on");
 });
+
+
+/* 함수 선언---------------------------------------- */
+//선택한 메뉴에 .on 추가
+function activation(lists, active) {
+  for (let el of lists) {
+    el.classList.remove(className_on);
+  }
+
+  active.currentTarget.classList.add(className_on);
+}
+
+//정렬 함수
+function sortFrame(target) {
+  const sort = target.currentTarget.querySelector("a").getAttribute("href");
+
+  grid.arrange({
+    filter: sort,
+  });
+}
+
+//팝업 함수
+function activePopUp(target) {
+  let tit = target.currentTarget.querySelector("h2").innerText;
+  let txt = target.currentTarget.querySelector("p").innerText;
+  let src = target.currentTarget.querySelector("img").getAttribute("src");
+
+  aside.querySelector("h2").innerText = tit;
+  aside.querySelector("p").innerText = txt;
+  aside.querySelector("img").setAttribute("src", src);
+
+  popUp.classList.add(className_on);
+}
